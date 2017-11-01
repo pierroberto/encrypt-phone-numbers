580c0997f54b96df7cc6bc5703268127a9ab3596
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-11-01 20:31:32 +0100

    style: removed console log and comments

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-
 module.exports = function encryptNumber (str, pos) {
-	var res = []
+	let res = []
   for (var i = str.length-1; i >= 0; i--) {
     if ( /\d/.test(str[i]) && pos) {
     res.push('X')
     pos--
-    } else {
-    res.push(str[i])
-    }
+    } else res.push(str[i])
   }
   return res.reverse().join('')
 }
-
-console.log(encryptNumber('+1-541-754-01/0', 4))
-// 754-3010 Local
-// (541) 754-3010 Domestic
-// +1-541-754-3010 International
-// 1-541-754-3010 Dialed in the US
