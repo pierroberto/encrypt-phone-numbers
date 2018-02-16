17798191e361286ea7f539c54aa7ef3525e78952
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-02-16 18:21:30 +0100

    Chore: check arguments passed in the function

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-module.exports = function encryptNumber (str, pos, symbol) {
-	let res = []
-  for (var i = str.length-1; i >= 0; i--) {
-    if ( /\d/.test(str[i]) && pos) {
-    res.push(symbol)
-    pos--
-    } else res.push(str[i])
+const encryptNumber = (str, pos, symbol) => {
+  if (!str || !pos || !symbol) return false;
+  if (pos < 1) return false;
+  let res = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (/\d/.test(str[i]) && pos) {
+      res.push(symbol);
+      pos--;
+    } else res.push(str[i]);
   }
-  return res.reverse().join('')
-}
+  return res.reverse().join("");
+};
+
+module.exports = { encryptNumber };
