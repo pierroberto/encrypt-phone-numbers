dd1c8a468fffddf64a6e3ee1dda76dc37b35109d
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-02-17 11:57:26 +0100

    Feat: support arrays of numbers

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const encryptNumber = (str, pos, symbol) => {
   if (!str || !pos || !symbol) return false;
   if (pos < 1) return false;
+  if (Array.isArray(str)) {
+    if (!str.length) return false;
+    return str.map(number => {
+      return hideNumbers(number, pos, symbol);
+    });
+  }
+  return hideNumbers(str, pos, symbol);
+};
+
+const hideNumbers = (str, pos, symbol) => {
   let res = [];
   for (let i = str.length - 1; i >= 0; i--) {
     if (/\d/.test(str[i]) && pos) {
